60927c2f61e30167cc516e3a2343ca1e4d078bb5
rog55 <rogehris@gmail.com>
2022-06-10 13:44:20 -0700

    First draft of rock paper scissors

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RockPaperScissors
+Created a game of rock paper scissors that relies on four functions.
+
+The first function generates a random selection from rock, paper, or scissors for the computer to play agaist the player
+
+The second function prompts the player to type in rock, paper, or scissors
+
+The third function takes two parameters, the computers selection and the players selection, and utilizes and if and else if statments to return whether the player tied, won, or lost to the computer.
+
+The final function is five rounds of rock paper scissors where the score is kept and a winner is proclaimed
---
+++ rps.js
@@ -0,0 +1,64 @@
+//Rock Paper Scissors Game
+//function 'computerPlay' to randomly generate computer input
+function computerPlay() {
+    //3 potential selections stored in an array
+    const selections = new Array('rock', 'paper', 'scissors');
+    //randomly select one of 3 choices from the array
+    const computerSelection = selections[Math.floor(Math.random() * 3)];
+    return computerSelection;
+}
+
+//prompt player to choose rock, paper or scissors
+function playerPlay() {
+    const playerSelection = prompt('Rock, paper, scissors SHOOT!!!\nChoose your fighter...');
+    //convert playerSelection to lower case
+    const lowerCasePlayerSelection = playerSelection.toLowerCase();
+    //confirm that the lowerCasePLayerSelection is one of the three options. I would prefer to have less code and combine the if and else if statements into if (lowerCasePlayerSelection === 'rock' || 'paper' || 'scissors'), but I cannot get it to work
+    if (lowerCasePlayerSelection === 'rock') {
+        return lowerCasePlayerSelection;
+    } else if (lowerCasePlayerSelection === 'paper') {
+        return lowerCasePlayerSelection;
+    } else if (lowerCasePlayerSelection === 'scissors') {
+        return lowerCasePlayerSelection;
+    //I would like this to bring you straight back to the original prompt after the alert, but cannot figure it out
+    } else {
+        alert('TRY AGAIN: rock, paper, or scissors?');
+    }
+}
+    
+//Play a single round of rock paper scissors using the two parameters computerSelection and playerSelection
+function playRound(playerSelection, computerSelection) {   
+    if (playerSelection === computerSelection) {
+        console.log('It\'s a tie! Play again.');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log('You lose! Paper beats Rock.');
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('You win! Rock beats scissors.');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log('You win! Paper beats rock.');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        console.log('You lose! Scissors beats paper.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('You win! Scissors beats paper.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log('You lose! Rock beats scissors.');
+    }
+}
+
+//call on playerPlay function to set the variable for playerSelection
+const playerSelection = playerPlay();
+//log detailed player choice
+console.log('Player chose: ' + playerSelection);
+//call on computerPlayfunction to randomly generate a variable for computerSelection
+const computerSelection = computerPlay();
+//log detailed computer choice
+console.log('Computer chose: ' + computerSelection)
+//initiate function with the two variables that were just set
+console.log(playRound(playerSelection, computerSelection));
+
+//function game() {
+    //for (let i = 0; i < 5; i++) {
+        //if (playRound() ===
+    //}
+//}
+    
